0b4d8b195797f71770e45da644753a0fbb83bacb
Mahipat Parmar <memahipat303@gmail.com>
2022-12-05 22:16:18 +0530

    modified

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,15 +1,15 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname) {
-  for (tablink of tablinks) {
-    tablink.classList.remove("active-link");
-  }
-  for (tabcontent of tabcontents) {
-    tabcontent.classList.remove("active-tab");
-  }
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
-}
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents = document.getElementsByClassName("tab-contents");
+// function opentab(tabname) {
+//   for (tablink of tablinks) {
+//     tablink.classList.remove("active-link");
+//   }
+//   for (tabcontent of tabcontents) {
+//     tabcontent.classList.remove("active-tab");
+//   }
+//   event.currentTarget.classList.add("active-link");
+//   document.getElementById(tabname).classList.add("active-tab");
+// }
 
 var sidemenu = document.getElementById("sidemenu");
 
