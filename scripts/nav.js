e031f9b52a645c48bc31deb19d32ba4b5e1488b1
Mahipat Parmar <memahipat303@gmail.com>
2022-12-19 11:41:29 +0530

    modified

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,16 +1,3 @@
-// var tablinks = document.getElementsByClassName("tab-links");
-// var tabcontents = document.getElementsByClassName("tab-contents");
-// function opentab(tabname) {
-//   for (tablink of tablinks) {
-//     tablink.classList.remove("active-link");
-//   }
-//   for (tabcontent of tabcontents) {
-//     tabcontent.classList.remove("active-tab");
-//   }
-//   event.currentTarget.classList.add("active-link");
-//   document.getElementById(tabname).classList.add("active-tab");
-// }
-
 var sidemenu = document.getElementById("sidemenu");
 
 var openmenu = () => {
@@ -19,5 +6,3 @@ var openmenu = () => {
 var closemenu = () => {
   sidemenu.style.right = "-200px";
 };
-
-
